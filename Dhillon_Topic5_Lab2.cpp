b1b35b6255c1ebe9d581f7faf7da41002aadee77
Karamveer Dhillon <karamveer2000@gmail.com>
2025-04-02 21:22:45 -0400

    Add file error handling and enhance output with min/max values

--- Dhillon_Topic5_Lab2.cpp
+++ Dhillon_Topic5_Lab2.cpp
@@ -15,17 +15,22 @@ int main() {
     // Used ofstream to create and open the file.
     ofstream file;
     file.open(your_Name + ".txt");
+    if (!file) {
+            cout << "File could not be opened!" << endl;
+            return 1;
+        }
     // Output my name to the file.
     file << your_Name << "\n";
 
     // Used time(0) in srand to get random vales everytime.
     srand(time(0));
+
+    // Declared constants for generating the random numbers.
+    const int MAX = 100;
+    const int MIN = 0;
+
     //Used a for loop to generate 50 random numbers between 0-100.
     for (int i = 0; i < 50; i++) {
-
-        // Declared constants for generating the random numbers.
-        const int MAX = 100;
-        const int MIN = 0;
 
         // Generated random numbers between 0-100.
         int random_Num = rand() % (MAX - MIN + 1) + MIN ;
@@ -42,31 +47,54 @@ int main() {
     // Used ifstream to open the file to read from it.
     ifstream in_File;
     in_File.open(your_Name + ".txt");
-    if(in_File.is_open()){
-        // Declared a string to hold the name read from the file.
-        string in_Name;
-        getline(in_File, in_Name);
-        // Out put the name in console.
-        cout << "The name in the file is: " << in_Name << endl;
 
-        //Declared a variable to store the value of the numbers read from the file
-        int in_Num;
-        int values_Read = 0;
-        double total = 0;
-        while(in_File >> in_Num){
-            values_Read++;
-            total += in_Num;
-            cout << left << setw(4) << in_Num;
-            if ((values_Read) % 2 == 0) cout << "\n";
+    if (!in_File) {
+            cout << "Error opening file for reading!" << endl;
+            return 1;
         }
-        cout << "The number of values read: " << values_Read << endl;
-        cout << "The total: " << total << endl;
-        cout <<
-        cout << fixed << setprecision(2) << "The average: " << (total/values_Read) << endl;
 
+    // Declared a string to hold the name read from the file.
+    string in_Name;
+    getline(in_File, in_Name);
+    // Out put the name in console.
+    cout << "\nThe name in the file is: \n" << in_Name << endl;
+
+    //Declared a variable to store the value of the numbers read from the file
+    int in_Num;
+    int values_Read = 0;
+    double total = 0;
+    int max_Value = MIN;
+    int min_Value = MAX;
+
+    while(in_File >> in_Num) {
+        // add 1 to the value of the numbers read from the file every loop.
+        values_Read++;
+        // add the value of the numbers read from the file every loop.
+        total += in_Num;
+
+        // used to check if the number is less than the current minimum
+        if (in_Num < min_Value) min_Value = in_Num;
+        // used to check if the number is greater than the current maximum
+        if (in_Num > max_Value) max_Value = in_Num;
+
+        // Output the number in console.
+        cout << left << setw(4) << in_Num;
+        // Added a new line after every 2 numbers.
+        if ((values_Read) % 2 == 0) cout << "\n";
     }
-    else cout << "File could not be opened !!"<< endl;
+    // Close the file after reading.
+    in_File.close();
+
+    // Calculated average;
+    double average = total / values_Read;
+    // Display the final values.
+    cout << "\nThe number of values read: " << values_Read << "\n";
+    cout << "The total: " << total << "\n";
+    cout << "The Largest number: " << max_Value << "\n";
+    cout << "The Smallest number: " << min_Value << "\n";
+    cout << fixed << setprecision(2) << "The average: " << average << endl;
 
 
+    // Return 0 to end the program.
     return 0;
 }
