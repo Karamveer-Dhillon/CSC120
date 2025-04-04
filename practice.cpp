e43af33178025e2256d8985680779367e60fb064
Karamveer Dhillon <karamveer2000@gmail.com>
2025-04-04 13:29:16 -0400

    Refactor MyStackClass implementation and remove unused pc_specs class

--- practice.cpp
+++ practice.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
-#include <ostream>
 #include <string>
 
-using namespace std;
+class MyStackClass {
+private:
+    int size;
+    std::string* stack;
 
-class pc_specs {
-    public:
-        string CPU;
-        string RAM;
-        string GPU;
-        pc_specs(string x,string y,string z) {
-        CPU = x;
-        RAM = y;
-        GPU = z;
+public:
+    MyStackClass() {
+        size = 5;
+        stack = new std::string[size];
+        stack[0] = "first";
+        stack[1] = "second";
+        stack[2] = "third";
+        stack[3] = "fourth";
+        stack[4] = "fifth";
+    }
+    void printStack() {
+        for (int i = 0; i < size; ++i) {
+            std::cout << stack[i] << std::endl;
         }
+    }
+
+    void push(const std::string& input) {
+        std::string* tempStack = new std::string[size + 1];
+        tempStack[0] = input;
+
+        for (int i = 0; i < size; ++i) {
+            tempStack[i + 1] = stack[i];
+        }
+
+        delete[] stack;         // Free old memory
+        stack = tempStack;      // Update pointer
+        size += 1;              // Update size
+    }
 };
 
 int main() {
-
-    pc_specs my_pc("Ryzen 5 7600","32GB","Nvidia RTX 4070ti Super");
-
-    cout << "CPU: "<< my_pc.CPU<< endl;
-    cout << "RAM: "<< my_pc.RAM<< endl;
-    cout << "GPU: "<< my_pc.GPU<< endl;
-
+    MyStackClass myStack;
+    myStack.printStack();
+    myStack.push("zero");
+    myStack.printStack();
     return 0;
 }
