32d30d42c86a3d988cdd1984db0aa177e3f5c067
Karamveer Dhillon <karamveer2000@gmail.com>
2025-04-06 01:47:32 -0400

    Add Java files to gitignore allowlist and create Main class

---
+++ Java/JavaPractice/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    public static void main(String[] args) {
+        add(1, 2);
+    }
+}
