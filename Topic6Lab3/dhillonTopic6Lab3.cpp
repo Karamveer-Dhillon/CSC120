ae58a80802b37b32713060d91bc5353c6e38b01d
karamveer <karamveer2000@icloud.com>
2025-04-16 20:47:30 -0400

    Refactor dhillonTopic6Lab3.cpp to enhance function usage and improve average calculation

--- Topic6Lab3/dhillonTopic6Lab3.cpp
+++ Topic6Lab3/dhillonTopic6Lab3.cpp
@@ -2,7 +2,9 @@
  Author: Karamveer Dhillon
  Date: 15 April 2025
  Filename:dhillonTopic6Lab3.cpp
- Description:
+ Description: This program demonstrates the use of functions in C++. It includes
+              functions to calculate the average of two integers, check if a number is odd,
+              and check if two or more numbers are the same.
 */
 #include <iostream>
 using namespace std;
@@ -12,31 +14,76 @@ double avg(int, int);
 void avg(int, int,double&);
 bool isOdd(int);
 bool isSame(int, int, int, int);
-// bool isSame(int,int);
+bool isSame(int,int);
 
-int main() {
- int sum = 33;
- int count = 10;
- double average = avg(sum, count);
- cout << "The average is " << average << endl;
+int main()
+{
+ int sum = 42;
+ int count = 11;
 
- cout << isOdd(32);
+ // call the functions
+ // Calling the double avg function.
+ double average = avg(sum,count);
+ cout << "The average is: " << average << endl;
+
+ // Calling the void average function.
+ sum = 43;
+ count = 12;
+
+ avg(sum, count, average);
+ cout << "The new average is: " << average << endl;
+
+ // Calling the isOdd function.
+ int value = 42;
+ cout << "The value " << value << " is odd: "
+ << (isOdd(value)? "True": "False") << endl;
+
+ // Calling the isSame functions.
+ int firstNum = 10;
+ int secondNum = 13;
+
+ cout << "The values of firstNum("<< firstNum
+ <<") and secondNum("<< secondNum<<") are same: "
+ << (isSame(firstNum,secondNum)? "True" : "False") << endl;
+
+ int thirdNum = 15;
+ int compareNum = 13;
+
+ cout <<"The compareNum("<<compareNum<<") is the same as firstNum("<<
+  firstNum<<"), secondNum("<<secondNum<<"), OR thirdNum("<<thirdNum<<"): "
+ << (isSame(firstNum,secondNum,thirdNum,compareNum)? "True" : "False");
+
+ return 0;
 }
-
 // Function definitions
 double avg(int sum, int count) {
  return static_cast<double>(sum) / count;
 }
-void avg(int sum, int count, double &average) {
+
+void avg(int sum, int count, double& average) {
  average = static_cast<double>(sum)/count;
 }
-bool isOdd(int x) {
-  if (x % 2 != 0) {
-   return true;
-  }
-  else {
-  return false;
-  }
+
+bool isOdd(int value) {
+  return value % 2 != 0;
 }
+
+bool isSame(int firstNum, int secondNum, int thirdNum, int compareNum ) {
+ if (isSame(firstNum, compareNum)) {
+  return isSame(firstNum,compareNum);
+ }
+ else if (isSame(secondNum, compareNum)) {
+  return isSame(secondNum,compareNum);
+ }
+ else if (isSame(thirdNum, compareNum)) {
+  return isSame(thirdNum,compareNum);
+ }
+ else return false;
+}
+
+bool isSame(int number, int compareNum) {
+ return number == compareNum;
+}
+
 
 
