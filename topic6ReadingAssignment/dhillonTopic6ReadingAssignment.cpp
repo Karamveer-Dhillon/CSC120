c67691eb6f105a61180ca594595c07aa90758f6c
Karamveer <Karamveer2000@icloud.com>
2025-04-19 18:16:16 -0400

    Add file metadata and include `using namespace std`
    Included author, date, filename, and description as metadata comments at the beginning of the file. Added `using namespace std` to simplify the code. Removed the example output code but left the main function as a placeholder.

--- topic6ReadingAssignment/dhillonTopic6ReadingAssignment.cpp
+++ topic6ReadingAssignment/dhillonTopic6ReadingAssignment.cpp
@@ -1,7 +1,12 @@
+/*
+ *Author: Karamveer Dhillon
+ *Date: 19 April 2025
+ *Filename: dhillonTopic6ReadingAssignment.cpp
+ *Description:
+*/
 #include <iostream>
+using namespace std;
 
-int main()
-{
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main() {
+
 }
