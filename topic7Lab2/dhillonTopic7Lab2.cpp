5f586c13d452901bfcc3a1c8232c2be58b279800
karamveer <karamveer2000@icloud.com>
2025-05-04 00:57:12 -0400

    Add Topic 7 Lab 2 implementation and .gitignore for IDE files
    Introduced a C++ program to generate and print random numbers in an array. The commit also includes an IDE-specific .gitignore file to exclude unnecessary files from version control.

---
+++ topic7Lab2/dhillonTopic7Lab2.cpp
@@ -0,0 +1,51 @@
+/*
+* Author: Karamveer Dhillon
+ * Date: April 28 2025
+ * Filename: dhillon_Topic7_Lab2.cpp
+ * Description: 
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+// Function to get a random  number between max and min.
+int randBetween(int MAX, int MIN)
+{
+  return (rand() % (MAX - MIN + 1)) + MIN;
+}
+// Function to fill random values to the array.
+void randArray(int array[],int size) {
+
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = randBetween(100, -100);
+  }
+}
+// Function to print the array
+void printArray(int array[],int size) {
+  for (int i = 0; i < size; i++)
+  {
+    cout << array[i];
+    cout << "\n";
+  }
+}
+int[] createArray(){
+
+}
+
+
+int main()
+{
+  //seeded the rand function with time
+  srand(time(0));
+  //Declared the array
+  const int ARRAYSIZE = 10;
+  int randNums[ARRAYSIZE];
+ //called the functions
+  randArray(randNums, ARRAYSIZE);
+  printArray(randNums, ARRAYSIZE);
+
+
+  return 0;
+}
